32d0652fbcbe43bfb96e965dc59849dbd1d24155
maximusnolte <noltemaximus43@gmail.com>
2025-10-17 14:14:00 +0200

    Vorlesung 01 und Übung00

--- Module/EPR/Vorlesungen/V01.py
+++ Module/EPR/Vorlesungen/V01.py
@@ -1,10 +1,10 @@
-#Geschrieben für: Python3.14
-from math import pi as pi #Nutzt math.pi für genauere Flächenberechnung
-r = input("Gib den Radius als eine Zahl ein: ") #Programm erhält eine Zahl (auch komma) als Eingabe
-try:
-    r = float(r) #Eingabe wird zu FLoat convertiert
-    A = r * r * pi #Formel der Kreis-Flächenberechnung wird angewandt
-    print("Die Fläche ist: " + str(A)) #Fertiges Ergebnis wird ausgegeben
-except ValueError: #Für Falscheingaben (Alles ausser eine normale Zahl)
-    print("Falsche Eingabe!")
+# Geschrieben für: Python3.14
+from math import pi as pi  # Nutzt math.pi für genauere Flächenberechnung
 
+r = input("Gib den Radius als eine Zahl ein: ")  # Programm erhält eine Zahl (auch komma) als Eingabe
+try:
+    r = float(r)  # Eingabe wird zu FLoat convertiert
+    A = r * r * pi  # Formel der Kreis-Flächenberechnung wird angewandt
+    print("Die Fläche ist: " + str(A))  # Fertiges Ergebnis wird ausgegeben
+except ValueError:  # Für Falscheingaben (Alles ausser eine normale Zahl)
+    print("Falsche Eingabe!")
---
+++ Module/EPR/Übungen/UE00.py
@@ -0,0 +1,46 @@
+__author__ = '<s5486426> , <Julian Maximus Nolte>'
+#! /venv/bin/python3.14
+
+#Python Programm, welches eine Bankleitzahl
+# und eine Kontonummer, sowie ein gültiges Länderkürzel entgegennimmt
+# und daraus die IBAN mit Prüfsumme bestimmt.
+
+
+land = input("Länderkürzel: ")
+bankleitzahl = input("Bankleitzahl: ")
+kontonummer = input("Kontonummer: ")
+if len(bankleitzahl) == 8 or len(kontonummer) <= 10:
+    try:
+        ibankleitzahl = int(bankleitzahl)
+        ikontonummer = int(kontonummer)
+    except ValueError:
+        print("Falsche Eingabe!")
+        quit()
+else:
+    print("Bankleitzahl oder Kontonummer ist nicht korrekt")
+    quit()
+
+if len(land) != 2:
+    print("Länderkürzel falsch! ")
+
+
+l_code = []
+for char in land:
+    l_code.append(ord(char)-55)
+
+pruefziffer = "00"
+
+output = bankleitzahl + kontonummer + str(l_code[0]) + str(l_code[1]) + pruefziffer
+#print(output)
+if len(output) == 24:
+    rest = int(output) % 97
+    pruefziffer = rest-98
+    pruefziffer = abs(pruefziffer)
+    #print(pruefziffer)
+else:
+    print("Fehler")
+
+output = land + str(pruefziffer) + " "+ bankleitzahl + " " + kontonummer
+print("\nIBAN:")
+print(output)
+
