aa6e3f38deb5ab1b873e8b5d4855fb226675c07b
maximusnolte <noltemaximus43@gmail.com>
2025-10-28 13:34:19 +0100

    GPR Ü02 Added

---
+++ Module/GPR/Übungen/Ü02_MaximusNolte.py
@@ -0,0 +1,17 @@
+__author__ = '8722674, Julian Maximus Nolte'
+#! /venv/bin/python3.14
+
+# Aufgabe 2, Handlungsvorschrift 1:
+
+a = input("Gib die erste Zahl a ein: ")
+b = input("Gib die zweite Zahl b ein: ")
+
+a = int(a)
+b = int(b)
+
+while b != 0:
+    h = a % b
+    a = b
+    b = h
+print("Der größte gemeinsame Teiler ist: ", a)
+
