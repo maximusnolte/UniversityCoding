b1ba45638611b4a59a54c155f844b813898e3da7
Maximus Nolte <97468165+maximusnolte@users.noreply.github.com>
2025-12-17 21:52:24 +0100

    Delete Module/EPR/Übungen/EPR_07/__main__.py

--- Module/EPR/Übungen/EPR_07/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Erlaube ``python -m Module.EPR.Übungen.EPR_07`` für die Kommandozeile."""
-from .main import main
-
-if __name__ == "__main__":  # pragma: no cover - Delegation an main()
-    raise SystemExit(main())
